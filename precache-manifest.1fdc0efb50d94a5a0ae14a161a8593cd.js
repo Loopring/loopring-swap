ea34515aee39aa0043a0e52f1f02e4f8dbf9c291
Daniel Wang <daniel@loopring.org>
2020-08-25 14:20:56 +0800

    Updates

---
+++ precache-manifest.1fdc0efb50d94a5a0ae14a161a8593cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44de4db7fb526e10e9ed783e731cdfab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b7f6fd85318d95583f4",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2b7f6fd85318d95583f4",
+    "url": "/static/js/2.ba226476.chunk.js"
+  },
+  {
+    "revision": "fb5f5e26ef8bb72c1b631fa441f46f50",
+    "url": "/static/js/2.ba226476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47126c09ff890c894319",
+    "url": "/static/js/main.d0e067a2.chunk.js"
+  },
+  {
+    "revision": "212e9dd3867a8b8655d6",
+    "url": "/static/js/runtime-main.e7ef9fec.js"
+  },
+  {
+    "revision": "22ea68168469694c736dd10d6c616a66",
+    "url": "/static/media/logo.22ea6816.png"
+  },
+  {
+    "revision": "6de5a91a5043b84ec07af42a51d3edb8",
+    "url": "/static/media/nest.6de5a91a.png"
+  },
+  {
+    "revision": "970c7ce6ff49a03e82a0a93c80bc8d4f",
+    "url": "/static/media/pnetwork.970c7ce6.png"
+  }
+]);
